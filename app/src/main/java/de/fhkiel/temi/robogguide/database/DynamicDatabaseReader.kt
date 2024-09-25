f86538096f633574414f05a2b8cdf41aa1c2cbc1
Hannes Eilers <hannes.eilers@fh-kiel.de>
2024-09-25 10:22:34 +0200

    Added using database file from assets

--- app/src/main/java/de/fhkiel/temi/robogguide/database/DynamicDatabaseReader.kt
+++ app/src/main/java/de/fhkiel/temi/robogguide/database/DynamicDatabaseReader.kt
@@ -3,16 +3,66 @@ package de.fhkiel.temi.robogguide.database
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
 import org.json.JSONObject
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
 
 class DynamicDatabaseReader(context: Context, databaseName: String) : SQLiteOpenHelper(context, databaseName, null, 1) {
 
-    override fun onCreate(db: SQLiteDatabase) {
-        // Example to create a table
-        db.execSQL("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age INTEGER)")
-    }
+    private val appContext = context
+
+    override fun onCreate(db: SQLiteDatabase) {}
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {}
+
+    private fun getFullDatabasePath(): String {
+        return "${appContext.packageName}/database/$databaseName"
+    }
+
+
+    // Method to copy the database from assets to internal storage
+    @Throws(IOException::class)
+    private fun copyDatabase() {
+        val inputStream: InputStream = appContext.assets.open(databaseName)
+        val outFileName = getFullDatabasePath()
+        val outputStream: OutputStream = FileOutputStream(outFileName)
+
+        val buffer = ByteArray(1024)
+        var length: Int
+        while (inputStream.read(buffer).also { length = it } > 0) {
+            outputStream.write(buffer, 0, length)
+        }
+
+        outputStream.flush()
+        outputStream.close()
+        inputStream.close()
+    }
+
+    // Method to check if the database already exists in internal storage
+    private fun checkDatabase(): Boolean {
+        var checkDB: SQLiteDatabase? = null
+        try {
+            checkDB = SQLiteDatabase.openDatabase(getFullDatabasePath(), null, SQLiteDatabase.OPEN_READONLY)
+        } catch (e: Exception) {
+            // Database does not exist yet
+            Log.e(appContext.packageName, "Database $databaseName not found!")
+        }
+
+        checkDB?.close()
+        return checkDB != null
+    }
+
+    // Method to initialize the database
+    @Throws(IOException::class)
+    fun initializeDatabase() {
+        if (!checkDatabase()) {
+            this.readableDatabase // Creates an empty database in the default system path
+            copyDatabase() // Copies the database from assets
+        }
+    }
 
     // Method to retrieve the table structure dynamically
     private fun getTableStructure(tableName: String): List<String> {
