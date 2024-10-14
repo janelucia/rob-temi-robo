c8f8ef212d9e649b6843c3f03ae4bd4e7f8f8f24
Hannes Eilers <hannes.eilers@fh-kiel.de>
2024-10-14 11:44:32 +0200

    Added doku

--- app/src/main/java/de/fhkiel/temi/robogguide/MainActivity.kt
+++ app/src/main/java/de/fhkiel/temi/robogguide/MainActivity.kt
@@ -1,7 +1,5 @@
 package de.fhkiel.temi.robogguide
 
-import android.content.pm.ActivityInfo
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -27,7 +25,7 @@ class MainActivity : AppCompatActivity(), OnRobotReadyListener, OnRequestPermiss
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // use database
+        // ---- DATABASE ACCESS ----
         val databaseName = "roboguide.db"
         database = DatabaseHelper(this, databaseName)
 
@@ -35,12 +33,13 @@ class MainActivity : AppCompatActivity(), OnRobotReadyListener, OnRequestPermiss
             database.initializeDatabase() // Initialize the database and copy it from assets
 
             /*
-            // EXAMPLE CODE TO ONLY COPY DATABASE TO DIRECTLY USE THE DATABASE FILE
+            // EXAMPLE CODE TO ONLY COPY DATABASE TO DIRECTLY USE THE DATABASE FILE FOR ORM
             database.initializeDatabase(withOpen = false)
             val dbFile = database.getDBFile()
             val sqLiteDatabase = database.getDatabase()
             */
 
+            // use json code to get database objects
             val places = database.getTableDataAsJson("places") // Fetch data as JSON
             val locations = database.getTableDataAsJson("locations") // Fetch data as JSON
             Log.i("MainActivity", "Places: $places")
@@ -52,7 +51,7 @@ class MainActivity : AppCompatActivity(), OnRobotReadyListener, OnRequestPermiss
 
         // let robot speak on button click
         findViewById<Button>(R.id.btnSpeakHelloWorld).setOnClickListener {
-            speakHelloWorld("Hello World!")
+            speakText("Hello World!")
         }
 
         findViewById<Button>(R.id.btnSpeakLocations).setOnClickListener {
@@ -75,11 +74,13 @@ class MainActivity : AppCompatActivity(), OnRobotReadyListener, OnRequestPermiss
     override fun onStart() {
         super.onStart()
         Robot.getInstance().addOnRobotReadyListener(this)
+        Robot.getInstance().addOnRequestPermissionResultListener(this)
     }
 
     override fun onStop() {
         super.onStop()
         Robot.getInstance().removeOnRobotReadyListener(this)
+        Robot.getInstance().removeOnRequestPermissionResultListener(this)
     }
 
     override fun onDestroy() {
@@ -90,6 +91,8 @@ class MainActivity : AppCompatActivity(), OnRobotReadyListener, OnRequestPermiss
     override fun onRobotReady(isReady: Boolean) {
         if (isReady){
             mRobot = Robot.getInstance()
+
+            // ---- DISABLE TEMI UI ELEMENTS ---
             // mRobot?.hideTopBar()        // hide top action bar
 
             // hide pull-down bar
@@ -101,28 +104,42 @@ class MainActivity : AppCompatActivity(), OnRobotReadyListener, OnRequestPermiss
     }
 
 
-    private fun speakHelloWorld(text: String, isShowOnConversationLayer: Boolean = true){
+    /**
+     * Speaks a text using the tmi tts
+     * @param text                          [String] text to speak
+     * @param isShowOnConversationLayer     [Boolean] true (default) to show conversation layer while speaking, false to hide it.
+     */
+    private fun speakText(text: String, isShowOnConversationLayer: Boolean = true){
         mRobot?.let { robot ->
             val ttsRequest: TtsRequest = TtsRequest.create(speech = text, isShowOnConversationLayer = isShowOnConversationLayer)
             robot.speak(ttsRequest)
         }
     }
 
+    /**
+     * Uses temi tts to speak every listed location on the active map
+     */
     private fun speakLocations(){
         mRobot?.let { robot ->
             var text = "Das sind alle Orte an die ich gehen kann:"
             robot.locations.forEach {
                 text += " $it,"
             }
-            speakHelloWorld(text, isShowOnConversationLayer = false)
+            speakText(text, isShowOnConversationLayer = false)
         }
     }
 
+    /**
+     * Uses temi sdk function to go to home base
+     */
     private fun gotoHomeBase(){
         mRobot?.goTo(location = "home base")
     }
 
 
+    /**
+     * Gets the [MapDataModel] of the robot and shows its data in Logcat
+     */
     private fun showMapData(){
         singleThreadExecutor.execute {
             getMap()?.let { mapDataModel ->
@@ -139,6 +156,11 @@ class MainActivity : AppCompatActivity(), OnRobotReadyListener, OnRequestPermiss
         Log.i( "Map-List", "${mRobot?.getMapList()}")
     }
 
+    /**
+     * Gets the robot [MapDataModel] data.
+     * Therefor it checks if the required permission is granted
+     * @return      [MapDataModel] of robots map data, or null if no robot found, no permissions are granted or data is null.
+     */
     private fun getMap(): MapDataModel? {
         // check if permission is missing
         if (requestPermissionsIfNeeded(Permission.MAP, REQUEST_CODE_MAP) == true){
@@ -151,6 +173,12 @@ class MainActivity : AppCompatActivity(), OnRobotReadyListener, OnRequestPermiss
     }
 
 
+    /**
+     * Requests a permission
+     * @param   permission      [Permission] robot temi permission to request
+     * @param   requestCode     [Int] request code to identify request response
+     * @return                  null if no robot accessible,  true if [Permission] was requested, false if [Permission] is already granted.
+     */
     @Suppress("SameParameterValue")
     private fun requestPermissionsIfNeeded(permission: Permission, requestCode: Int): Boolean? {
         return mRobot?.let { robot ->
@@ -164,6 +192,12 @@ class MainActivity : AppCompatActivity(), OnRobotReadyListener, OnRequestPermiss
         }
     }
 
+    /**
+     * Permission request callback
+     * @param   permission      [Permission] that was requested.
+     * @param   grantResult     [Int] with request result (see [Permission] constants)
+     * @param   requestCode     [Int] custom request code set in [Permission] request
+     */
     override fun onRequestPermissionResult(
         permission: Permission,
         grantResult: Int,
