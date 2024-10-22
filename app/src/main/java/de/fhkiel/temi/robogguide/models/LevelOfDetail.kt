e083ddb97bc12700d63b23fc7e74ac7a017a4df4
sihingbenni <sihing.benni@gmx.de>
2024-10-22 13:38:28 +0200

    ADD Level of Detail enum

---
+++ app/src/main/java/de/fhkiel/temi/robogguide/models/LevelOfDetail.kt
@@ -0,0 +1,30 @@
+package de.fhkiel.temi.robogguide.models
+
+/**
+ * Enum class for the level of detail of a tour
+ * The parameters need to be filled at a later point, when the db is analyzed.
+ * These Level of Details need to be set for each new Place.
+ *
+ * @param lengthInMinutes the length of the tour in minutes
+ * @param nrOfExhibits the number of exhibits in the tour
+ */
+enum class LevelOfDetail(var lengthInMinutes: Int?, var nrOfExhibits: Int?) {
+    EVERYTHING_DETAILED(null,null),
+    EVERYTHING_CONCISE(null, null),
+    ONLY_IMPORTANT_DETAILED(null, null),
+    ONLY_IMPORTANT_CONCISE(null, null);
+
+    fun getLengthInMinutes(): Int {
+        if (lengthInMinutes == null) {
+            throw IllegalStateException("Length in minutes is not set for ${this.name}!")
+        }
+        return lengthInMinutes!!
+    }
+
+    fun nrOfExhibits(): Int {
+        if (nrOfExhibits == null) {
+            throw IllegalStateException("Nr of Exhibits is not set for ${this.name}!")
+        }
+        return nrOfExhibits!!
+    }
+}
