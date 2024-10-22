b9b56da091efbd62168831ce9d825eb86ad826ce
sihingbenni <sihing.benni@gmx.de>
2024-10-22 13:44:10 +0200

    ADD Place model

---
+++ app/src/main/java/de/fhkiel/temi/robogguide/models/Place.kt
@@ -0,0 +1,9 @@
+package de.fhkiel.temi.robogguide.models
+
+class Place(
+    private val name: String,
+    private val unimportantLocations: List<Location>,
+    private val importantLocations: List<Location>
+) {
+
+}
