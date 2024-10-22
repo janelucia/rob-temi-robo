187cab8de688e667c3346cf1cec26b4e858d4cdc
sihingbenni <sihing.benni@gmx.de>
2024-10-22 13:45:07 +0200

    ADD models for Location and Tour

---
+++ app/src/main/java/de/fhkiel/temi/robogguide/models/Location.kt
@@ -0,0 +1,5 @@
+package de.fhkiel.temi.robogguide.models;
+
+class Location(private val name: String) {
+
+}
---
+++ app/src/main/java/de/fhkiel/temi/robogguide/models/Tour.kt
@@ -0,0 +1,5 @@
+package de.fhkiel.temi.robogguide.models
+
+class Tour(private val place: Place, private val levelOfDetail: LevelOfDetail) {
+
+}
