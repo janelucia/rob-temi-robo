05a83b81014612f2c72e25815aa7d834eef80509
karlina.jung <karlina.jung@student.fh-kiel.de>
2024-10-22 21:39:33 +0200

    rm: unused imports

--- app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/pages/Feedback.kt
+++ app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/pages/Feedback.kt
@@ -1,6 +1,5 @@
 package de.fhkiel.temi.robogguide.ui.theme.pages
 
-import android.widget.ImageButton
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -9,22 +8,17 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import de.fhkiel.temi.robogguide.R
 import de.fhkiel.temi.robogguide.ui.theme.components.Header
