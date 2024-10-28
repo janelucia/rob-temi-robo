9e1f20029ef6d220d446998a4207eba1254feaa5
karlina.jung <karlina.jung@student.fh-kiel.de>
2024-10-28 10:51:47 +0100

    mod: top bar shows current exhibit (PLACEHOLDER) and it's centered!!!

--- app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/components/CustomTopAppBar.kt
+++ app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/components/CustomTopAppBar.kt
@@ -16,8 +16,13 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -29,59 +34,74 @@ fun CustomTopAppBar(navController: NavController) {
     val navBackStackEntry = navController.currentBackStackEntryAsState()
     val currentDestination = navBackStackEntry.value?.destination?.route
 
-    TopAppBar(
-        title = { Row {
-            Image(
-                imageVector = ImageVector.vectorResource(id = R.drawable.fachhochschule_kiel_logo_03_2022),
-                contentDescription = "Fachhochschule Kiel Logo",
-                modifier = Modifier.size(200.dp),
-                contentScale = ContentScale.Fit
-            )
-        }},
-        actions = {
-
-            if (currentDestination == "guide") {
-                GuideProgressBar()
-            }
-
-            if (currentDestination != "homePage") {
-                IconButton(
-                    onClick = { navController.navigate("homePage") },
-                    modifier = Modifier.size(50.dp)
-                        .border(2.dp, Color.Black, CircleShape)
-                        .clip(CircleShape)
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.Home,
-                        contentDescription = "Home",
-                        tint = Color.Black
+    Box(modifier = Modifier.fillMaxWidth()) {
+        TopAppBar(
+            title = {
+                Row {
+                    Image(
+                        imageVector = ImageVector.vectorResource(id = R.drawable.fachhochschule_kiel_logo_03_2022),
+                        contentDescription = "Fachhochschule Kiel Logo",
+                        modifier = Modifier.size(200.dp),
+                        contentScale = ContentScale.Fit
                     )
                 }
-            }
-            Spacer(modifier = Modifier.width(16.dp))
-            CustomButton(
-                title = "EN",
-                onClick = {},
-                width = 100.dp,
-                height = 50.dp,
-                fontSize = 32.sp,
-                backgroundColor = Color.White,
-                contentColor = Color.Black,
-                borderColor = Color.Black,
-                borderWidth = 2.dp,
-            )
-            Spacer(modifier = Modifier.width(16.dp))
-            CustomButton(
-                title = "?",
-                onClick = {},
-                width = 60.dp,
-                height = 55.dp,
-                fontSize = 32.sp,
-                backgroundColor = Color.Black,
-                contentColor = Color.White,
-                borderColor = Color.Black,
-                borderWidth = 2.dp,
+            },
+            actions = {
+                if (currentDestination != "homePage") {
+                    IconButton(
+                        onClick = { navController.navigate("homePage") },
+                        modifier = Modifier
+                            .size(50.dp)
+                            .border(2.dp, Color.Black, CircleShape)
+                            .clip(CircleShape)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Home,
+                            contentDescription = "Home",
+                            tint = Color.Black
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.width(16.dp))
+                CustomButton(
+                    title = "EN",
+                    onClick = {},
+                    width = 100.dp,
+                    height = 50.dp,
+                    fontSize = 32.sp,
+                    backgroundColor = Color.White,
+                    contentColor = Color.Black,
+                    borderColor = Color.Black,
+                    borderWidth = 2.dp,
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                CustomButton(
+                    title = "?",
+                    onClick = {},
+                    width = 60.dp,
+                    height = 55.dp,
+                    fontSize = 32.sp,
+                    backgroundColor = Color.Black,
+                    contentColor = Color.White,
+                    borderColor = Color.Black,
+                    borderWidth = 2.dp,
+                )
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        if (currentDestination == "guide") {
+            // Exponat Titel anzeigen
+            Header(
+                title = "AKTUELLES EXPONAT",
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                modifier = Modifier
+                    .align(Alignment.Center) // Header mittig auf dem GESAMTEN BILDSCHIRM platzieren
+                    .padding(top = 8.dp)
+
             )
         }
-    )
+
+    }
 }
