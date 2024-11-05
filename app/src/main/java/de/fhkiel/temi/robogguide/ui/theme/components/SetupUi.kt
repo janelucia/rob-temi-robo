5a6e482e1ed575c8f06a49f87447ae5c57ed545a
Benjamin Puhani <benjamin.puhani@student.fh-kiel.de>
2024-11-05 15:41:01 +0100

    FIX remove unnecessary import

--- app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/components/SetupUi.kt
+++ app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/components/SetupUi.kt
@@ -1,6 +1,5 @@
 package de.fhkiel.temi.robogguide.ui.theme.components
 
-import android.util.Log
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
