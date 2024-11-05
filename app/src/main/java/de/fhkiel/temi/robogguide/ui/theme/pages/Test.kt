d7dd7d8a93101ec0f8de1902ed9256f398087c68
Benjamin Puhani <benjamin.puhani@student.fh-kiel.de>
2024-11-05 15:50:28 +0100

    MOD Print Detailed Text

--- app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/pages/Test.kt
+++ app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/pages/Test.kt
@@ -1,11 +1,24 @@
 package de.fhkiel.temi.robogguide.ui.theme.pages
 
+import android.util.Log
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import com.robotemi.sdk.Robot
 import de.fhkiel.temi.robogguide.logic.TourManager
+import de.fhkiel.temi.robogguide.models.Location
+import de.fhkiel.temi.robogguide.ui.theme.components.TextDisplay
 
 @Composable
 fun Test(mRobot: Robot?, tourManager: TourManager) {
-
-
+    Column {
+        tourManager.selectedPlace?.allLocations?.forEach { location: Location ->
+            Log.d("Test", "Location: ${location.name}")
+            location.conciseText?.let {
+                TextDisplay(text = it)
+            }
+            location.detailedText?.let {
+                TextDisplay(text = it)
+            }
+        }
+    }
 }
