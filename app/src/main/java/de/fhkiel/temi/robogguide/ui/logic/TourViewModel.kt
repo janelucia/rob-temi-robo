a89bb2d63c2d231e3bc11e78e2bc8ae56554e73a
karlina.jung <karlina.jung@student.fh-kiel.de>
2024-11-05 23:06:31 +0100

    add: getter and setter for locations and currentexhibit

--- app/src/main/java/de/fhkiel/temi/robogguide/ui/logic/TourViewModel.kt
+++ app/src/main/java/de/fhkiel/temi/robogguide/ui/logic/TourViewModel.kt
@@ -4,12 +4,30 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.lifecycle.ViewModel
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import de.fhkiel.temi.robogguide.models.Location
 
 class TourViewModel : ViewModel() {
     private var currentExhibit by mutableIntStateOf(0)
+    private var tourLocations: List<Location> = emptyList()
+
+    val numberOfExhibits: Int = tourLocations.size
+
+    fun setTourLocations(locations: List<Location>) {
+        tourLocations = locations
+    }
+
+    fun getTourLocations(): List<Location> {
+        return tourLocations
+    }
 
     fun setCurrentExhibit(index: Int) {
-        currentExhibit = index
+        if (index in 0 until numberOfExhibits) {
+            currentExhibit = index
+        }
+    }
+
+    fun getCurrentExhibit(): Int {
+        return currentExhibit
     }
 }
 
