6e633320dff90f0abd38f2d3aa170f885d22c3ad
Benjamin Puhani <benjamin.puhani@student.fh-kiel.de>
2024-11-05 15:41:09 +0100

    FIX remove unnecessary import

--- app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/pages/Test.kt
+++ app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/pages/Test.kt
@@ -1,13 +1,8 @@
 package de.fhkiel.temi.robogguide.ui.theme.pages
 
-import android.util.Log
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
 import com.robotemi.sdk.Robot
 import de.fhkiel.temi.robogguide.logic.TourManager
-import de.fhkiel.temi.robogguide.ui.theme.components.TextDisplay
 
 @Composable
 fun Test(mRobot: Robot?, tourManager: TourManager) {
