1a13daceb8e42b3b7e909608092deaf8c49b6d3b
Karlina Jung <karlina.jung@student.fh-kiel.de>
2024-11-07 18:49:30 +0100

    rm: not working stuff

--- app/src/main/java/de/fhkiel/temi/robogguide/logic/SpeakText.kt
+++ app/src/main/java/de/fhkiel/temi/robogguide/logic/SpeakText.kt
@@ -2,15 +2,9 @@ package de.fhkiel.temi.robogguide.logic
 
 import com.robotemi.sdk.Robot
 import com.robotemi.sdk.TtsRequest
-import kotlinx.coroutines.delay
 
-var isSpeaking = false
 
 fun robotSpeakText(mRobot: Robot?, text: String?, isShowOnConversationLayer: Boolean = true) {
-
-    if (isSpeaking) {
-        return
-    }
 
     text?.let { txt ->
         mRobot?.let { robot ->
@@ -18,7 +12,6 @@ fun robotSpeakText(mRobot: Robot?, text: String?, isShowOnConversationLayer: Boo
                 speech = txt,
                 isShowOnConversationLayer = isShowOnConversationLayer
             )
-            isSpeaking = true
             robot.speak(ttsRequest)
         }
 
