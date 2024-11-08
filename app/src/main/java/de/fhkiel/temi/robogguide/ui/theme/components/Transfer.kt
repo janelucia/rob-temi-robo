f4c6d18b92e3a980b64c15cf093ae77c890075cd
jane <janeluciaschoenfeld@gmail.com>
2024-11-08 12:18:53 +0100

    fix: show image on transfer

--- app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/components/Transfer.kt
+++ app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/components/Transfer.kt
@@ -1,26 +1,17 @@
 package de.fhkiel.temi.robogguide.ui.theme.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.rememberAsyncImagePainter
 import com.robotemi.sdk.Robot
-import de.fhkiel.temi.robogguide.R
-import de.fhkiel.temi.robogguide.logic.robotSpeakText
 import de.fhkiel.temi.robogguide.models.Item
-import de.fhkiel.temi.robogguide.models.LevelOfDetail
 import de.fhkiel.temi.robogguide.ui.logic.TourViewModel
-import kotlinx.coroutines.delay
 
 @Composable
 fun Transfer(currentItem: Item, mRobot: Robot?, tourViewModel: TourViewModel) {
@@ -36,10 +27,10 @@ fun Transfer(currentItem: Item, mRobot: Robot?, tourViewModel: TourViewModel) {
         fontSize = 32.sp
     )
     currentItem.conciseText?.media?.let { media ->
-        Image(
-            painter = rememberAsyncImagePainter(model = media.url),
-            contentDescription = null,
-            modifier = Modifier.fillMaxWidth(),
+        LoadingImage(
+            urlString = media.url.toString(),
+            modifier = Modifier
+                .fillMaxWidth(),
             contentScale = ContentScale.Crop
         )
     }
