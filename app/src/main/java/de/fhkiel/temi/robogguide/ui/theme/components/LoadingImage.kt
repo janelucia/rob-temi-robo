72d43cc49357fd2154709a31223741e387fa1fab
Benjamin Puhani <benjamin.puhani@student.fh-kiel.de>
2024-11-10 00:16:34 +0100

    MOD use Box for DisplayMediaList

--- app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/components/LoadingImage.kt
+++ app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/components/LoadingImage.kt
@@ -1,7 +1,10 @@
 package de.fhkiel.temi.robogguide.ui.theme.components
 
 import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
@@ -11,21 +14,30 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import coil.compose.AsyncImage
 import coil.request.ErrorResult
 import de.fhkiel.temi.robogguide.R
 
 @Composable
-fun LoadingImage(urlString: String, modifier: Modifier = Modifier.fillMaxWidth(), contentScale: ContentScale = ContentScale.FillBounds) {
+fun LoadingImage(
+    urlString: String,
+    modifier: Modifier = Modifier.fillMaxWidth(),
+    contentScale: ContentScale = ContentScale.FillBounds
+) {
 
     var isLoading by remember { mutableStateOf(false) }
+    var showDialog by remember { mutableStateOf(false) }
+
     Box(contentAlignment = Alignment.Center, modifier = modifier) {
         AsyncImage(
             model = urlString,
             contentDescription = "Exponat Image",
-            modifier = modifier,
+            modifier = modifier.clickable(onClick = { showDialog = true }),
             contentScale = contentScale,
             onLoading = {
                 isLoading = true
@@ -46,4 +58,29 @@ fun LoadingImage(urlString: String, modifier: Modifier = Modifier.fillMaxWidth()
             CircularProgressIndicator()
         }
     }
+
+    if (showDialog) {
+        Dialog(
+            onDismissRequest = { showDialog = false },
+            properties = DialogProperties(usePlatformDefaultWidth = false)
+        ) {
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black)) {
+                AsyncImage(
+                    model = urlString,
+                    contentDescription = "Fullscreen Image",
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Fit
+                )
+                CustomIconButton(
+                    iconId = R.drawable.baseline_cancel_24,
+                    onClick = { showDialog = false },
+                    modifier = Modifier.align(Alignment.TopEnd)
+
+                )
+            }
+        }
+    }
+
 }
