4bf631a050d7dc8ab953ad0698586b0460fa194c
Benjamin Puhani <benjamin.puhani@student.fh-kiel.de>
2024-11-10 00:05:12 +0100

    MOD use Box for DisplayMediaList

--- app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/components/DisplayMediaList.kt
+++ app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/components/DisplayMediaList.kt
@@ -3,18 +3,24 @@ package de.fhkiel.temi.robogguide.ui.theme.components
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import de.fhkiel.temi.robogguide.models.MediaType
 import de.fhkiel.temi.robogguide.models.Text
 
 /**
@@ -31,17 +37,23 @@ fun DisplayMediaList(vararg texts: Text?) {
         texts.forEach { text ->
             text?.mediaList?.forEach { media ->
                 item {
-                    if (media.url.toString().isEmpty()) {
-                        StockImage()
-                    } else if (media.url.host == "www.youtube.com") {
-                        Log.i("Exhibit", "Video kann ich noch nicht")
-                    } else {
-                        LoadingImage(
-                            urlString = media.url.toString(),
-                            modifier = Modifier.size(400.dp),
-                            contentScale = ContentScale.FillBounds
-                        )
-                        Spacer(modifier = Modifier.width(16.dp))
+                    Box (
+                        modifier = Modifier.padding(16.dp).size(400.dp)
+                    ) {
+                        when (media.type) {
+                            MediaType.VIDEO -> {
+                                Log.d("DisplayMediaList", "Displaying video: ${media.url}")
+                                //TODO: Implement video player
+                            }
+                            MediaType.IMAGE -> {
+                                Log.d("DisplayMediaList", "Displaying image: ${media.url}")
+                                LoadingImage(
+                                    urlString = media.url.toString(),
+                                    modifier = Modifier.fillMaxSize(),
+                                    contentScale = ContentScale.FillBounds
+                                )
+                            }
+                        }
                     }
                 }
             }
