f9802848669b95b2a724030a4110d23bb3877483
jane <janeluciaschoenfeld@gmail.com>
2024-11-10 14:01:00 +0100

    feat: add error dialog

--- app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/components/PopUp.kt
+++ app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/components/PopUp.kt
@@ -8,14 +8,17 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import androidx.navigation.NavController
+import com.robotemi.sdk.Robot
+import de.fhkiel.temi.robogguide.logic.robotSpeakText
 
 @Composable
 fun HelpPopup(onDismiss: () -> Unit, activity: Activity) {
-
     Dialog(onDismissRequest = onDismiss) {
         Box(
             modifier = Modifier
@@ -122,6 +125,144 @@ fun ConfirmationPopUp(onDismiss: () -> Unit, onConfirm: () -> Unit, title: Strin
                     height = 50.dp,
                     fontSize = 32.sp
                 )
+            }
+        }
+    }
+}
+
+@Composable
+fun ClosePopup(onDismiss: () -> Unit, navController: NavController, mRobot: Robot?) {
+    Dialog(
+        onDismissRequest = { onDismiss() },
+        content = {
+            Box(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(Color.White)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(32.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Möchtest du die Tour wirklich beenden?",
+                        fontSize = 32.sp,
+                        lineHeight = 32.sp
+                    )
+                    Spacer(modifier = Modifier.padding(16.dp))
+                    Text(
+                        "Dadurch wird die Tour beendet und du wirst zum Startbildschirm zurückgeleitet.",
+                        fontSize = 24.sp,
+                        lineHeight = 24.sp
+                    )
+                    Spacer(modifier = Modifier.padding(16.dp))
+                    CustomButton(
+                        onClick = {
+                            navController.navigate("homePage")
+                            onDismiss()
+                        },
+                        title = "Zum Startbildschirm",
+                        modifier = Modifier.fillMaxWidth(),
+                        height = 100.dp,
+                        fontSize = 24.sp,
+                        initialBackgroundColor = Color.White,
+                        contentColor = Color.Black
+                    )
+                    Spacer(modifier = Modifier.padding(16.dp))
+                    CustomButton(
+                        onClick = {
+                            onDismiss()
+                            mRobot.let { robot ->
+                                robotSpeakText(mRobot, "Ich fahre jetzt zur Aufladestation!")
+                                robot?.goTo("home base")
+                            }
+                        },
+                        title = "Roboter zurück zur Ladestation schicken",
+                        modifier = Modifier.fillMaxWidth(),
+                        height = 100.dp,
+                        fontSize = 24.sp,
+                        initialBackgroundColor = Color.White,
+                        contentColor = Color.Black
+                    )
+                    Spacer(modifier = Modifier.padding(16.dp))
+                    CustomButton(
+                        onClick = {
+                            onDismiss()
+                        },
+                        title = "Tour fortsetzen",
+                        modifier = Modifier.fillMaxWidth(),
+                        height = 100.dp,
+                        fontSize = 24.sp
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun ErrorPopUp(onDismiss: () -> Unit, title: String, message: String, onClick: () -> Unit, navController: NavController, mRobot: Robot?) {
+    robotSpeakText(mRobot, message)
+    Dialog(
+        onDismissRequest = onDismiss,
+    ) {
+        Box(
+            modifier = Modifier
+                .background(Color.White, shape = RoundedCornerShape(16.dp))
+                .padding(16.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Header(
+                    title,
+                    fontSize = 32.sp,
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    message,
+                    fontSize = 24.sp,
+                    lineHeight = 32.sp
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                ) {
+                    CustomButton(
+                        onClick = {
+                            onDismiss()
+                            onClick()
+                        },
+                        title = "Erneut versuchen",
+                        width = 400.dp,
+                        height = 50.dp,
+                        fontSize = 32.sp
+                    )
+                    Spacer(modifier = Modifier.height(32.dp))
+                    CustomButton(
+                        onClick = {
+                            onDismiss
+                            navController.navigate("homePage")
+                            mRobot?.goTo("home base") },
+                        title = "Zur Ladestation fahren",
+                        width = 400.dp,
+                        height = 50.dp,
+                        fontSize = 32.sp,
+                        initialBackgroundColor = Color.White,
+                        contentColor = Color.Black
+                    )
+                    Spacer(modifier = Modifier.height(32.dp))
+                    CustomButton(
+                        onClick = onDismiss,
+                        title = "Schließen",
+                        width = 400.dp,
+                        height = 50.dp,
+                        fontSize = 32.sp
+                    )
+                }
             }
         }
     }
