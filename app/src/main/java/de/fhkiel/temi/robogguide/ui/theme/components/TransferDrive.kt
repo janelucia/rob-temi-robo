ac762592c7f1312c361c08c21b3781c8c33b0259
Benjamin Puhani <benjamin.puhani@student.fh-kiel.de>
2024-11-10 19:49:13 +0100

    MOD Temi face

--- app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/components/TransferDrive.kt
+++ app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/components/TransferDrive.kt
@@ -1,9 +1,13 @@
 package de.fhkiel.temi.robogguide.ui.theme.components
 
 import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -11,6 +15,7 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
@@ -92,12 +97,27 @@ fun TransferDrive(
     )
     currentLocation.conciseText?.mediaList?.first().let { media ->
         // Bei Transfers gibt es nur ein Bild, da wir nur wissen müssen, wo es hingeht
-        LoadingImage(
-            urlString = media?.url.toString(),
-            modifier = Modifier
-                .fillMaxWidth(),
-            contentScale = ContentScale.FillHeight
-        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Box(
+                modifier = Modifier.width(1300.dp),
+                contentAlignment = Alignment.CenterEnd
+            ) {
+                LoadingImage(
+                    urlString = media?.url.toString(),
+                    modifier = Modifier.align(Alignment.CenterEnd).width(800.dp),
+                    contentScale = ContentScale.FillHeight
+                )
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Box(
+                modifier = Modifier.width(500.dp).fillMaxHeight(),
+            ){
+                TemiSpeakingFace()
+            }
+        }
+
     }
 
 
