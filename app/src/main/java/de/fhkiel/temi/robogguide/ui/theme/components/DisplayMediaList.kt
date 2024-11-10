1793c9b131d0a38f30bb162ce272ccec719e9f01
Benjamin Puhani <benjamin.puhani@student.fh-kiel.de>
2024-11-10 16:23:41 +0100

    ADD Youtube Player

--- app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/components/DisplayMediaList.kt
+++ app/src/main/java/de/fhkiel/temi/robogguide/ui/theme/components/DisplayMediaList.kt
@@ -1,24 +1,15 @@
 package de.fhkiel.temi.robogguide.ui.theme.components
 
 import android.util.Log
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import de.fhkiel.temi.robogguide.models.MediaType
@@ -43,7 +34,11 @@ fun DisplayMediaList(vararg texts: Text?) {
                         when (media.type) {
                             MediaType.VIDEO -> {
                                 Log.d("DisplayMediaList", "Displaying video: ${media.url}")
-                                //TODO: Implement video player
+                                LoadingVideo(
+                                    url = media.url,
+                                    modifier = Modifier.fillMaxSize(),
+                                    contentScale = ContentScale.FillBounds
+                                )
                             }
                             MediaType.IMAGE -> {
                                 Log.d("DisplayMediaList", "Displaying image: ${media.url}")
