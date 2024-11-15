18d7c46ece54174d9b957ac7ecdb800249d5defb
jane <janeluciaschoenfeld@gmail.com>
2024-11-15 15:17:26 +0100

    chore: add documentation for GuideState

--- app/src/main/java/de/fhkiel/temi/robogguide/models/GuideState.kt
+++ app/src/main/java/de/fhkiel/temi/robogguide/models/GuideState.kt
@@ -1,5 +1,8 @@
 package de.fhkiel.temi.robogguide.models
 
+/**
+ * GuideState: Enum class to represent the state of the guide from start to end
+ */
 enum class GuideState {
     TransferStart, // robot leads guest to next exhibit
     TransferGoing,
